9210cce9ee201f0fcb60168db3f3a4e925db2cd7
Manish Songirkar <manish.songirkar@rtcamp.com>
2013-12-24 19:13:36 +0530

    Removed unwanted scripts

--- assets/rtwidgets-scripts.js
+++ assets/rtwidgets-scripts.js
@@ -1,20 +1,9 @@
+/**
+ * rtWidgets Script Library
+ * 
+ * @since rtWidget 1.0
+ */
+
 jQuery(document).ready(function() {
     jQuery( '#rtw-login-container' ).tabs();
-    
-//    jQuery( '.tab_content_login' ).hide();
-//    jQuery( 'ul.tabs_login li:first' ).addClass( 'active' ).show();
-//    jQuery( '.tab_content_login:first' ).show();
-//    jQuery( 'ul.tabs_login li' ).click(function() {
-//        jQuery( 'ul.tabs_login li' ).removeClass( 'active' );
-//        jQuery(this).addClass( 'active' );
-//        jQuery( '.tab_content_login' ).hide();
-//        var activeTab = jQuery(this).find( 'a' ).attr( 'href' );
-//        if (jQuery.browser.msie) {
-//            jQuery(activeTab).show();
-//        }
-//        else {
-//            jQuery(activeTab).show();
-//        }
-//        return false;
-//    });
 });
